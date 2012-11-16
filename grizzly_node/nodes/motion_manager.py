a5b2148524f43e52517e23719a8a5ef43951825d
Ryan Gariepy <rgariepy@clearpathrobotics.com>
2012-11-16 11:01:45 -0500

    Adding motion manager, adding e-stop to teleop, now broadcasting zeros when teleop is not enabled (will need to revert this)

---
+++ grizzly_node/nodes/motion_manager.py
@@ -0,0 +1,237 @@
+#!/usr/bin/python
+
+# Delays incoming twist messages, ensures that they ramp smoothly
+# Sets off beeper during delay interval and as appropriate during motion
+
+#
+# Logic:
+#
+# Republish cmd_vel to safe_cmd_vel according to the independent
+# translational and acceleration ramps. 
+#
+# If any of the following conditions are met, or have been in the last X 
+# seconds, do not publish at all:
+# - We have not received cmd_vel for >10 s
+# - We are seeing an error >1 on the status from the AVR (error=1 is a timeout)
+# - We have just started up
+#
+# When we are republishing, pulse beacon and turn headlights to solid
+# When we are in the X second startup phase, pulse beacon faster, pulse
+# beeper, and flash head and taillamps
+# When we are going backwards, turn taillamps to solid
+#
+
+import roslib; roslib.load_manifest('grizzly_node')
+import rospy
+from math import copysign
+
+from geometry_msgs.msg import Twist
+from grizzly_msgs.msg import RawStatus, Ambience
+
+
+class MotionManager:
+    def __init__(self):
+        rospy.init_node('motion_manager')
+
+        # Delay time [s]
+        self.motion_delay = rospy.get_param('~motion_delay', 2)
+   
+        # Max trans speed [m/s]
+        self.trans_speed = rospy.get_param('~trans_speed', 2)
+ 
+        # Max rot speed [rad/s]
+        self.rot_speed = rospy.get_param('~rot_speed', 2)
+ 
+        # Max trans acceleration [m/s^2]
+        self.trans_accel = rospy.get_param('~trans_accel', 0.5)
+        self.trans_decel = rospy.get_param('~trans_decel', 2)
+    
+        # Max rot acceleration [rad/s^2]
+        self.rot_accel = rospy.get_param('~rot_accel', 0.5)
+        self.rot_decel = rospy.get_param('~rot_accel', 2)
+
+        # Command timeout [s]
+        self.no_cmd_timeout = rospy.get_param('~no_cmd_timeout', 0.1)
+
+        # Motion timeout [s]
+        self.no_motion_timeout = rospy.get_param('~no_motion_timeout', 10)
+
+        # No-motion value [m/s & rad/s]
+        self.no_motion_value = rospy.get_param('~no_motion_value',0.01)
+
+        # Publishers & subscribers
+        self.cmd_vel = rospy.Publisher('safe_cmd_vel', Twist)
+
+        self.cmd_lights = rospy.Publisher('ambience', Ambience)
+
+        rospy.Subscriber("cmd_vel", Twist, self.vel_callback)
+        rospy.Subscriber("grizzly_raw_status", RawStatus, self.status_callback)
+
+        # Timing
+        self.rate = rospy.Rate(rospy.get_param('~hz',50))
+        self.period = 1.0/rospy.get_param('~hz',50)
+
+        # Time at which the last command was received [s]
+        self.last_cmd_time = 0
+
+        # Time at which the last command with a nonzero motion was received [s]
+        self.last_motion_time = 0
+
+        # Last reference setpoint received
+        self.cmd_ref = Twist()
+
+        # Last command sent
+        self.cmd = Twist() 
+    
+        # Time at which we last had an error
+        self.last_error_time = 0 
+
+        # Explicit errors?
+        self.control_error = False
+
+        # Start main state machine
+        self.loop()
+
+    def loop(self):
+
+        # State variable
+        # 0 = Error/startup state
+        # 1 = No error, but still counting down
+        # 2 = Normal behaviour (ramped velocity, lights)
+        motion_state = 0
+
+        while not rospy.is_shutdown():
+            """ Main state machine loop """
+            if motion_state == 0:
+                # State: 
+                # Update last error time
+                self.last_error_time = rospy.get_time()
+                # Reset last command setpoint
+                self.cmd = Twist()
+                self.cmd_vel.publish(self.cmd) 
+                # Nothing on ambience
+                noises = Ambience()
+                self.cmd_lights.publish(noises)
+
+                # Leave error state?
+                if not self.is_error():
+                    motion_state = 1
+                    continue
+            elif motion_state == 1:
+                # State: 
+                rospy.loginfo("Vehicle Startup In Process")
+                # Reset last command setpoint
+                self.cmd = Twist()
+                self.cmd_vel.publish(self.cmd) 
+                # Flash lights and make noises appropriately
+                noises = Ambience()
+                noises.beacon = Ambience.PATTERN_DFLASH
+                noises.headlight = Ambience.PATTERN_DFLASH
+                noises.taillight = Ambience.PATTERN_DFLASH
+                noises.beep = Ambience.PATTERN_FLASH
+                self.cmd_lights.publish(noises)
+
+                # Leave warning state?
+                # -> Error
+                if self.is_error():
+                    rospy.loginfo("Vehicle Startup Interrupted")
+                    motion_state = 0
+                    continue
+                # -> Motion
+                if rospy.get_time() > self.last_error_time + self.motion_delay:
+                    rospy.loginfo("Vehicle Startup Complete")
+                    motion_state = 2
+                    continue
+            elif motion_state == 2:
+                brakelight = False
+                # State:
+                # Update setpoints, saturating with acceleration
+                # Allow faster absolute deceleration
+                # TODO: Unit test the acceleration profile
+                # TODO: Move to callback?
+
+                # Translational
+                delta = self.cmd_ref.linear.x - self.cmd.linear.x
+                if abs(self.cmd_ref.linear.x) > abs(self.cmd.linear.x):
+                    # We are increasing our target speed
+                    accel = self.trans_accel
+                else:
+                    # We are decreasing our target speed
+                    accel = self.trans_decel
+                delta = copysign(min(abs(delta), accel*self.period), delta)
+                self.cmd.linear.x += delta
+                if delta < 0:
+                    brakelight = True
+                
+                # Rotational
+                delta = self.cmd_ref.angular.z - self.cmd.angular.z
+                if abs(self.cmd_ref.angular.z) > abs(self.cmd.angular.z):
+                    # We are increasing our target speed
+                    accel = self.rot_accel
+                else:
+                    # We are decreasing our target speed
+                    accel = self.rot_decel
+                delta = copysign(min(abs(delta), accel*self.period), delta)
+                self.cmd.angular.z += delta
+                self.cmd_vel.publish(self.cmd)
+
+                # Flash lights and make noises appropriately
+                noises = Ambience()
+                noises.beacon = Ambience.PATTERN_FLASH
+                noises.headlight = Ambience.PATTERN_ON
+                if brakelight or self.cmd.linear.x < 0:
+                    noises.taillight = Ambience.PATTERN_ON
+                else:
+                    noises.taillight = Ambience.PATTERN_OFF
+                self.cmd_lights.publish(noises)
+
+                # Leave standard state?
+                # -> Error
+                if self.is_error():
+                    motion_state = 0
+                    continue
+            self.rate.sleep()
+
+    def is_error(self):
+        """ Check all error states """
+        # Explicit error from board
+        if self.control_error:
+            self.last_error_time = rospy.get_time()
+            rospy.loginfo("Board Control Error")
+            return True
+        # Haven't moved for a while
+        if rospy.get_time() > self.last_motion_time + self.no_motion_timeout:
+            self.last_error_time = rospy.get_time()
+            rospy.loginfo("Command Timeout Error (No Motion)")
+            return True
+        return False
+   
+    def status_callback(self, data):
+        """ Receive status. If we have a suitable error, flag """
+        # TODO: Use bitfields properly
+        if data.error > 1:
+            self.control_error = True
+        else:
+            self.control_error = False
+ 
+    def vel_callback(self, data):
+        """ Receive incoming Twist message """
+        self.last_cmd_time = rospy.get_time()
+        # If the value is non-zero-ish, record that we are seeing
+        # valid commands
+        # TODO: Do we want to use encoders to check motion instead?
+        if abs(data.linear.x) > self.no_motion_value or \
+           abs(data.angular.z) > self.no_motion_value:
+            self.last_motion_time = rospy.get_time()
+        # TODO: Turn this into an object with rest of motion profile?
+        # Saturate against speed limits
+        x = min(data.linear.x, self.trans_speed)
+        x = max(x, -self.trans_speed)
+        z = min(data.angular.z, self.rot_speed)
+        z = max(z, -self.rot_speed)
+        self.cmd_ref = Twist()
+        self.cmd_ref.linear.x = x
+        self.cmd_ref.angular.z = z
+
+if __name__ == "__main__": 
+    MotionManager()
--- grizzly_teleop/nodes/teleop.py
+++ grizzly_teleop/nodes/teleop.py
@@ -1,10 +1,15 @@
 #!/usr/bin/python
 
+""" If we're using the usual Logitech gamepad, suggest using "X" mode
+via the switch on the front. That will map the buttons to "Green=GO, 
+Red=E-Stop"""
+
 import roslib; roslib.load_manifest('grizzly_teleop')
 import rospy
 
 from sensor_msgs.msg import Joy
 from geometry_msgs.msg import Twist
+from std_msgs.msg import Bool
 
 class Teleop:
     def __init__(self):
@@ -13,9 +18,12 @@ class Teleop:
         self.turn_scale = rospy.get_param('~turn_scale')
         self.drive_scale = rospy.get_param('~drive_scale')
         self.deadman_button = rospy.get_param('~deadman_button', 0)
+        self.estop_button = rospy.get_param('~estop_button', 1)
 
         self.cmd = None
         cmd_pub = rospy.Publisher('cmd_vel', Twist)
+        self.estop = False
+        cmd_estop = rospy.Publisher('stop', Bool)
 
         rospy.Subscriber("joy", Joy, self.callback)
         rate = rospy.Rate(rospy.get_param('~hz', 20))
@@ -24,6 +32,16 @@ class Teleop:
             rate.sleep()
             if self.cmd:
                 cmd_pub.publish(self.cmd)
+            else:
+                # TODO: Move this out to motion_manager
+                # This will add robustness to motion_manager
+                cmd_pub.publish(Twist())
+            """ As long as we're active, give it e-stop data
+            A manual reset is required once an e-stop is asserted
+            anyway, and having to reset everytime we let go of the
+            joystick isn't the point """
+            cmd_estop.publish(self.estop)
+        
 
     def callback(self, data):
         """ Receive joystick data, formulate Twist message. """
@@ -35,7 +53,11 @@ class Teleop:
             self.cmd = cmd
         else:
             self.cmd = None
+        if data.buttons[self.estop_button] == 1:
+            self.estop = True
+        else:
+            self.estop = False
 
 
 if __name__ == "__main__": 
-    Teleop()
+    Teleop() 
