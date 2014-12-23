19efb1ed249915a6cac73d01c75ecfea52cfa888
Shokoofeh Pourmehr <spourmehr@clearpathrobotics.com>
2014-12-23 16:16:40 -0500

    Fix grizzly_motion/tests bugs

--- grizzly_motion/test/dead_reckoning_tests.cpp
+++ grizzly_motion/test/dead_reckoning_tests.cpp
@@ -11,17 +11,18 @@ TEST(DeadReckoning, simple_straight)
 
   DeadReckoning dr(1.0, wheel_radius);
   nav_msgs::Odometry odom;
+  sensor_msgs::JointState joints;
   grizzly_msgs::DrivePtr encoders(new grizzly_msgs::Drive);
   encoders->header.stamp = start;
   encoders->front_left = encoders->rear_left = 1.1;
   encoders->front_right = encoders->rear_right = 1.1;  // rad/s
-  dr.next(encoders, &odom);
+  dr.next(encoders, &odom, &joints);
 
   // 10 seconds of travel at 50 Hz
   for(int step = 0; step < 500; step++)
   {
     encoders->header.stamp += ros::Duration(0.02);
-    EXPECT_TRUE(dr.next(encoders, &odom));
+    EXPECT_TRUE(dr.next(encoders, &odom, &joints));
   } 
 
   // Expected travel is time * rad/s * wheel radius
@@ -44,17 +45,18 @@ TEST(DeadReckoning, curved_path)
 
   DeadReckoning dr(vehicle_width, wheel_radius);
   nav_msgs::Odometry odom;
+  sensor_msgs::JointState joints;
   grizzly_msgs::DrivePtr encoders(new grizzly_msgs::Drive);
   encoders->header.stamp = start;
   encoders->front_left = encoders->rear_left = left_travel / wheel_radius / 10.0;
   encoders->front_right = encoders->rear_right = right_travel / wheel_radius / 10.0;
-  dr.next(encoders, &odom);
+  dr.next(encoders, &odom, &joints);
 
   // 10 seconds of travel
   for(int step = 0; step < 500; step++)
   {
     encoders->header.stamp += ros::Duration(0.02);
-    EXPECT_TRUE(dr.next(encoders, &odom));
+    EXPECT_TRUE(dr.next(encoders, &odom, &joints));
     ROS_DEBUG_STREAM("ENC " << encoders->front_left << " " << encoders->front_right);
     ROS_DEBUG_STREAM("POSE " << odom.pose.pose.position.x << " " << odom.pose.pose.position.y);
   } 
@@ -72,19 +74,20 @@ TEST(DeadReckoning, timeout)
   ros::Time start = ros::Time::now();
   
   nav_msgs::Odometry odom;
+  sensor_msgs::JointState joints;
   grizzly_msgs::DrivePtr encoders(new grizzly_msgs::Drive);
   encoders->header.stamp = start;
 
   DeadReckoning dr(1.0, 1.0);
-  dr.next(encoders, &odom);
+  dr.next(encoders, &odom, &joints);
 
   // This one should not time out.
   encoders->header.stamp += ros::Duration(0.09);
-  EXPECT_TRUE(dr.next(encoders, &odom));
+  EXPECT_TRUE(dr.next(encoders, &odom, &joints));
 
   // This one should.
   encoders->header.stamp += ros::Duration(0.11);
-  EXPECT_FALSE(dr.next(encoders, &odom));
+  EXPECT_FALSE(dr.next(encoders, &odom, &joints));
 }
 
 int main(int argc, char **argv)
